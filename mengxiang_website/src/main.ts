7eb7396e68d36180d53f5a953001ee2847fb948e
zjxllla <1978678141@qq.com>
2025-03-20 13:10:37 +0800

    add:禁止首页文本选中

--- mengxiang_website/src/main.ts
+++ mengxiang_website/src/main.ts
@@ -5,7 +5,7 @@ import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import vSlidein from './Instruction/vSlidein'
-
+import './assets/main.css'
 const app = createApp(App)
 
 app.directive('slidein', vSlidein)
