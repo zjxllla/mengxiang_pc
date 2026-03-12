169d0a7e6c853eb1792089ae5b00ca625dd1f9a9
darlingggg <3316906761@qq.com>
2026-03-12 20:51:26 +0800

    fix: 添加环境变量

--- mengxiang_website/env.d.ts
+++ mengxiang_website/env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  // 在这里声明你的环境变量
+  readonly VITE_API_URL: string
+}
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
--- mengxiang_website/src/axios/index.ts
+++ mengxiang_website/src/axios/index.ts
@@ -2,8 +2,7 @@ import axios from 'axios'
 import { useGlobalStore } from '../stores'
 const globalStore = useGlobalStore()
 const instance = axios.create({
-  // baseURL: 'http://101.201.170.43:8080',
-  baseURL: 'http://localhost:8080',
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
